8d08ef3c302771933e0ee0e2407e7bbb14f3957a
Johnny Prencipe <johnny.prencipe@gmail.com>
2020-12-05 23:29:24 +0000

    added feature to read account value based on transaction history

--- index.js
+++ index.js
@@ -1,13 +1,53 @@
 let balance = 500.00;
 
-class Withdrawal {
+class Transaction {
 
-  constructor(amount) {
+  constructor(amount, account) {
     this.amount = amount;
+    this.account = account;
   }
 
   commit() {
-    balance -= this.amount;
+    this.time = new Date();
+    this.account.addTransaction(this);
+  }
+
+}
+
+class Account {
+
+  constructor(username) {
+    this.username = username;
+    this.transactions = [];
+  }
+
+  addTransaction(transaction) {
+    this.transactions.push(transaction);
+  }
+
+  get balance() {
+    let counter = 0
+    this.transactions.forEach(transaction => {
+      counter += transaction.value;
+    });
+    return counter;
+  }
+
+
+}
+
+class Withdrawal extends Transaction {
+
+  get value() {
+    return -this.amount;
+  }
+
+}
+
+class Deposit extends Transaction {
+
+  get value() {
+    return this.amount;
   }
 
 }
@@ -18,12 +58,20 @@ class Withdrawal {
 // DRIVER CODE BELOW
 // We use the code below to "drive" the application logic above and make sure it's working as expected
 
-t1 = new Withdrawal(50.25);
+const myAccount = new Account('snow-patrol');
+
+console.log('Starting balance', myAccount.balance);
+
+t1 = new Withdrawal(50.25, myAccount);
 t1.commit();
 console.log('Transaction 1:', t1);
 
-t2 = new Withdrawal(9.99);
+t2 = new Withdrawal(9.99, myAccount);
 t2.commit();
 console.log('Transaction 2:', t2);
 
-console.log('Balance:', balance);
+t3 = new Deposit(120.00, myAccount);
+t3.commit();
+console.log('Transaction 3:', t3);
+
+console.log('Ending balance', myAccount.balance);
